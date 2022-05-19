dac7a5fcf017a0755df9aa5bfc4d3bd0e8a2d634
Art83 <shvetsov1983@gmail.com>
2022-05-19 16:59:03 +1000

    first attempt

--- string_sum.go
+++ string_sum.go
@@ -2,6 +2,8 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
@@ -23,5 +25,53 @@ var (
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
-	return "", nil
+	var sum int
+	var i int = 0
+	var num string
+	cnt := 0
+	if len(input) == 0 {
+		return "", fmt.Errorf("error happened: %w", errorEmptyInput)
+	}
+	for i < len(input) {
+		if string(input[i]) == " " || string(input[i]) == "+" {
+			i++
+		} else {
+			if string(input[i]) == "-" {
+				num = num + string("-")
+				i++
+				for string(input[i]) != " " && string(input[i]) != "-" && string(input[i]) != "+" {
+					_, err := strconv.Atoi(string(input[i]))
+					if err != nil {
+						return "", fmt.Errorf("error happened: %w", errorNotTwoOperands)
+					}
+					num = num + string(input[i])
+					i++
+					if i >= len(input) {
+						break
+					}
+				}
+				cnt++
+			} else {
+				for string(input[i]) != " " && string(input[i]) != "-" && string(input[i]) != "+" {
+					_, err := strconv.Atoi(string(input[i]))
+					if err != nil {
+						return "", fmt.Errorf("error happened: %w", errorNotTwoOperands)
+					}
+					num = num + string(input[i])
+					i++
+					if i >= len(input) {
+						break
+					}
+				}
+				cnt++
+			}
+			int_num, _ := strconv.Atoi(num)
+			sum += int_num
+		}
+		num = ""
+	}
+	if cnt != 2 {
+		return "", fmt.Errorf("error happened: %w", errorNotTwoOperands)
+	}
+	return strconv.Itoa(sum), nil
 }
